983ff5cae082dfebc1a593d9ec1622a1e4dbb5be
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-07 18:41:05 -0700

    drinks database setup

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+//Set the 'drinks database' to a variable in server.js
+const drinks = require("./models/drinks.js")
 
 
 app.get("/", (req, res) => {
     res.send("Welcome to the GitPub App!")
+})
+
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
 })
 
 app.listen(PORT, () => {
