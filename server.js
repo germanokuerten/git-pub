a12461f559b17c51c17d8e7ae27e5812631b7971
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-12 17:44:21 -0700

    comment updates

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-// require("dotenv").config()
+//////////////////////
+// GitPub app
+//////////////////////
+
+require("dotenv").config()
 
 const express = require("express")
 const app = express()
